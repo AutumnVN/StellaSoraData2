34d997829d76b2eaa4ab46014ce902404417c694
MakoStar <makostar.github@gmail.com>
2026-07-31 04:06:25 +0000

    chore(CN): auto-update lua v127 (p2) [c1.13.0_g1.13.1_p2]

--- _Lua/Game/UI/Avg/_cn/Preset/AvgCharacter.lua
+++ _Lua/Game/UI/Avg/_cn/Preset/AvgCharacter.lua
@@ -1163,6 +1163,12 @@ return {
 		ver = "1.9.0"
 	},
 	{
+		id = "avg3_217",
+		name = "***",
+		name_bg_color = "#fffdf5",
+		ver = "1.13.2"
+	},
+	{
 		id = "avg3_218",
 		name = "米拉什居民男",
 		name_bg_color = "#0ABEC5",
@@ -1195,19 +1201,19 @@ return {
 	{
 		id = "avg3_223",
 		name = "***",
-		name_bg_color = "#a5cdc3",
+		name_bg_color = "#ecd09f",
 		ver = "1.15.0"
 	},
 	{
 		id = "avg3_224",
 		name = "***",
-		name_bg_color = "#0ABEC5",
+		name_bg_color = "#73a4c8",
 		ver = "1.15.0"
 	},
 	{
 		id = "avg3_225",
 		name = "***",
-		name_bg_color = "#0ABEC5",
+		name_bg_color = "#fffcdf",
 		ver = "2.0.0"
 	},
 	{
@@ -1250,7 +1256,7 @@ return {
 		id = "avg3_232",
 		name = "***",
 		name_bg_color = "#0ABEC5",
-		ver = "2.1.0"
+		ver = "1.13.2"
 	},
 	{
 		id = "avg3_233",
